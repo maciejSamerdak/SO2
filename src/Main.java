d22c7e5bea8cec7ff44f5492d18a6e6de5e179f2
maciejSamerdak <sam.maciej@interia.pl>
2016-04-26 12:54:55 +0200

    Super duper nowy wariant.

--- src/Main.java
+++ src/Main.java
@@ -58,35 +58,40 @@ public class Main {
 		
 		//domyœlnie iloœæ takich zg³oszeñ to liczba losowa z przedzia³u od 4 do iloœæ zg³oszeñ/2
 		int rtReffQuant=rn.nextInt(reffQuantity/2-4)+4;
+		int x=0;
 		for (int i=0; i<rtReffQuant; i++){
 			int pos=rn.nextInt(size);
 			
 			while(disk1.containsRef(pos))
 				pos=rn.nextInt(size);
 			
+			
+			int arrival=x+rn.nextInt(5);
+			x+=5;
+			
 			//System.out.println(pos);
 			
 			int deadline=rn.nextInt(size-1)+1;
 			
-			disk1.refferences.insert(new Rekord(pos, deadline));
-			disk12.refferences.insert(new Rekord(pos, deadline));
-			disk13.refferences.insert(new Rekord(pos, deadline));
+			//disk1.refferences.insert(new Rekord(pos, deadline, arrival));
+			disk12.refferences.insert(new Rekord(pos, deadline, arrival));
+			disk13.refferences.insert(new Rekord(pos, deadline, arrival));
 			
-			disk2.refferences.insert(new Rekord(pos, deadline));
-			disk22.refferences.insert(new Rekord(pos, deadline));
-			disk23.refferences.insert(new Rekord(pos, deadline));
+			//disk2.refferences.insert(new Rekord(pos, deadline, arrival));
+			disk22.refferences.insert(new Rekord(pos, deadline, arrival));
+			disk23.refferences.insert(new Rekord(pos, deadline, arrival));
 			
-			disk3.refferences.insert(new Rekord(pos, deadline));
-			disk32.refferences.insert(new Rekord(pos, deadline));
-			disk33.refferences.insert(new Rekord(pos, deadline));
+			//disk3.refferences.insert(new Rekord(pos, deadline, arrival));
+			disk32.refferences.insert(new Rekord(pos, deadline, arrival));
+			disk33.refferences.insert(new Rekord(pos, deadline, arrival));
 			
-			disk4.refferences.insert(new Rekord(pos, deadline));
-			disk42.refferences.insert(new Rekord(pos, deadline));
-			disk43.refferences.insert(new Rekord(pos, deadline));
+			//disk4.refferences.insert(new Rekord(pos, deadline, arrival));
+			disk42.refferences.insert(new Rekord(pos, deadline, arrival));
+			disk43.refferences.insert(new Rekord(pos, deadline, arrival));
 		}
 		//System.out.println("kontrola");	
 		//tu podstawiamy wyniki z EDF i FD-SCAN, do pózniejszego odniesienia w tabeli wyników
-		int[]fordisk12=EDF(disk12);
+		/*int[]fordisk12=EDF(disk12);
 		int[]fordisk13=FDSCAN(disk13);
 		
 		int[]fordisk22=EDF(disk22);
@@ -96,47 +101,72 @@ public class Main {
 		int[]fordisk33=FDSCAN(disk33);
 			
 		int[]fordisk42=EDF(disk42);
-		int[]fordisk43=FDSCAN(disk43);
+		int[]fordisk43=FDSCAN(disk43);*/
 		
 		//drukujemy dane i wyniki
 		System.out.printf("Liczba bloków: %d; Liczba zg³oszeñ: %d; Liczba zg³oszeñ Real-Time: %d; Pierwotne po³o¿enie g³owic: %d;", size, reffQuantity, rtReffQuant, defaultPos);
 		System.out.print("\n");
-		System.out.printf("|%10s|%10s|%10s|%10s|%10s|%10s|", "Algorytm", "Standard", "Z EDF", "Odrzucenia", "Z FD-SCAN", "Odrzucenia");
+		System.out.printf("|%10s|%10s|%10s|%10s|", "Algorytm", "Standard", "Z EDF", /*"Odrzucenia",*/ "Z FD-SCAN"/*, "Odrzucenia"*/);
 		System.out.print("\n");
-		System.out.printf("|%-10s|%10d|%10d|%10d|%10s|%10s|", "FCFS", FCFS(disk1, disk1.defaultPos), fordisk12[0]+FCFS(disk12,fordisk12[2]), fordisk12[1], fordisk13[0]+FCFS(disk13,fordisk13[2]), fordisk13[1]);
+		System.out.printf("|%-10s|%10d|%10d|%10d|", "FCFS", FCFS(disk1, false, false), FCFS(disk12, true, false), /*fordisk12[1],*/ FCFS(disk13, false, true)/*, fordisk13[1]*/);
 		System.out.print("\n");
-		System.out.printf("|%-10s|%10d|%10d|%10d|%10s|%10s|", "SSTF", SSTF(disk2, disk2.defaultPos), fordisk22[0]+SSTF(disk12,fordisk22[2]), fordisk22[1], fordisk23[0]+0, fordisk23[1]);
+		System.out.printf("|%-10s|%10d|%10d|%10d|", "SSTF", SSTF(disk2, false, false), /*fordisk22[0]+*/SSTF(disk22, true, false), /*fordisk22[1],*/ /*fordisk23[0]+*/SSTF(disk23, false, true)/*, fordisk23[1]*/);
 		System.out.print("\n");
-		System.out.printf("|%-10s|%10d|%10d|%10d|%10s|%10s|", "SCAN", SCAN(disk3, disk3.defaultPos), fordisk32[0]+SCAN(disk12,fordisk32[2]), fordisk32[1], fordisk33[0]+SCAN(disk33,fordisk33[2]), fordisk33[1]);
+		System.out.printf("|%-10s|%10d|%10d|%10d|", "SCAN", SCAN(disk3, false, false), /*fordisk32[0]+*/SCAN(disk12, true, false), /*fordisk32[1],*/ /*fordisk33[0]+*/SCAN(disk33, false, true)/*, fordisk33[1]*/);
 		System.out.print("\n");
-		System.out.printf("|%-10s|%10d|%10d|%10d|%10s|%10s|", "C-SCAN", CSCAN(disk4, disk4.defaultPos), fordisk42[0]+CSCAN(disk12,fordisk42[2]), fordisk42[1], fordisk43[0]+CSCAN(disk43,fordisk43[2]), fordisk43[1]);
+		System.out.printf("|%-10s|%10d|%10d|%10d|", "C-SCAN", CSCAN(disk4, false, false), /*fordisk42[0]+*/CSCAN(disk12, true, false), /*fordisk42[1],*/ /*fordisk43[0]+*/CSCAN(disk43, false, true)/*, fordisk43[1]*/);
 	}
 	
 	/*we wszystkich algorytmach czas zliczamy na podstawie liczby bloków, przez które g³owica musi przejœæ do celu
 	 * czas = | aktualna pozycja - pozycja celu |
 	 */
-	public static int FCFS(HDD disk, int pos){
+	public static int FCFS(HDD disk, boolean edf, boolean fdscan){
 		int time=0;
-		int currentPos=pos;
+		int currentPos=disk.defaultPos;
 		Rekord temp;
 		while(!disk.refferences.isEmpty()){ //dopóki kolejka jest pe³na realizujemy kolejne zg³oszenia
+			
+			if(edf || fdscan){
+				int[] result;
+				if(edf)
+					result=EDF(disk, currentPos, time);
+				else
+					result=FDSCAN(disk, currentPos, time);
+				currentPos=result[2];
+				time=result[0];
+			}
+			
 			temp=(Rekord)disk.refferences.getValue(1);
 			time+=Math.abs(currentPos-temp.getPos());
 			currentPos=temp.getPos();
 			disk.refferences.removeFirst();
+			disk.updateDeadlines(time);
 		}
 		return time;
 	}
 	
-	public static int SCAN(HDD disk, int pos){
+	public static int SCAN(HDD disk, boolean edf, boolean fdscan){
 		int time=0;
 		
-		int currentPos=pos;
+		int currentPos=disk.defaultPos;
 		//idziemy w dó³ do samego koñca realizuj¹c napotkane zadania
 		for (int i=currentPos; i>=0; i--){
-			if(disk.removeRefAt(i)){
+			
+			if(edf || fdscan){
+				int[] result;
+				if(edf)
+					result=EDF(disk, currentPos, time);
+				else
+					result=FDSCAN(disk, currentPos, time);
+				currentPos=result[2];
+				time=result[0];
+			}
+			
+			if(disk.removeRefAt(i,time)){
 				time+=Math.abs(currentPos-i);
 				currentPos=i;
+				disk.updateDeadlines(time);
+				
 			}
 			
 		}
@@ -144,57 +174,104 @@ public class Main {
 		currentPos=0;
 		//idziemy w górê do samego koñca realizuj¹c napotkane zadania
 		for (int i=currentPos; i<disk.getSize(); i++){
-			if(disk.removeRefAt(i)){
+			
+			if(edf || fdscan){
+				int[] result;
+				if(edf)
+					result=EDF(disk, currentPos, time);
+				else
+					result=FDSCAN(disk, currentPos, time);
+				currentPos=result[2];
+				time=result[0];
+			}
+			
+			if(disk.removeRefAt(i,time)){
 				time+=Math.abs(currentPos-i);
 				currentPos=i;
+				disk.updateDeadlines(time);
 			}
 		}
 		time+=Math.abs(disk.getSize()-1-currentPos);
 		return time;
 	}
 
-	public static int CSCAN(HDD disk, int pos){
+	public static int CSCAN(HDD disk, boolean edf, boolean fdscan){
 		int time=0;
 		//idziemy w dó³ do samego koñca realizuj¹c napotkane zadania
-		int currentPos=pos;
+		int currentPos=disk.defaultPos;
 		for (int i=currentPos; i>=0; i--){
-			if(disk.removeRefAt(i)){
+			
+			if(edf || fdscan){
+				int[] result;
+				if(edf)
+					result=EDF(disk, currentPos, time);
+				else
+					result=FDSCAN(disk, currentPos, time);
+				currentPos=result[2];
+				time=result[0];
+			}
+			
+			if(disk.removeRefAt(i,time)){
 				time+=Math.abs(currentPos-i);
 				currentPos=i;
+				disk.updateDeadlines(time);
 			}			
 		}
 		//idziemy na drugi koniec
 		time+=currentPos+disk.getSize()-1;
 		currentPos=disk.getSize()-1;
 		//znów idziemy w dó³ do pozycji startowej realizuj¹c napotkane zadania
-		for (int i=currentPos; i>pos; i--){
-			if(disk.removeRefAt(i)){
+		for (int i=currentPos; i>disk.defaultPos; i--){
+			
+			if(edf || fdscan){
+				int[] result;
+				if(edf)
+					result=EDF(disk, currentPos, time);
+				else
+					result=FDSCAN(disk, currentPos, time);
+				currentPos=result[2];
+				time=result[0];
+			}
+			
+			if(disk.removeRefAt(i, time)){
 				time+=Math.abs(currentPos-i);
 				currentPos=i;
+				disk.updateDeadlines(time);
 			}
 		}
-		time+=Math.abs(pos-currentPos);
+		time+=Math.abs(disk.defaultPos-currentPos);
 		return time;
 	}
 	
-	public static int SSTF(HDD disk, int pos){
+	public static int SSTF(HDD disk, boolean edf, boolean fdscan ){
 		int time=0;
-		int currentPos=pos;
+		int currentPos=disk.defaultPos;
 		int i,j=0;
 		i=currentPos+1; j=currentPos-1;
 		boolean zn1=false, zn2=false; int zn3=0, zn4=0;
-		for (int l=0; l<reffQuantity; l++){
+		while (!disk.refferences.isEmpty()){
+			
+			if(edf || fdscan){
+				int[] result;
+				if(edf)
+					result=EDF(disk, currentPos, time);
+				else
+					result=FDSCAN(disk, currentPos, time);
+				currentPos=result[2];
+				time=result[0];
+			}
+			
 			//System.out.println("currentPos= " + currentPos);
 			while(i>=0 && zn1==false){
 				i--;
-				if(disk.containsRef(i)){
+				if(disk.containsRef(i) && disk.getRef(i).getDeadline()<=time){
 					zn1=true;
 				}
 			}
 			//System.out.println("i= " + i);
 			while(j<disk.getSize() && zn2==false){
 				j++;
-				if(disk.containsRef(j)){
+				if(disk.containsRef(j) && disk.getRef(j).getDeadline()<=time){
 					zn2=true;
 				}
 			}
@@ -220,18 +297,19 @@ public class Main {
 		return time;
 	}
 	
-	public static int[] EDF(HDD disk){
-		int currentPos=disk.defaultPos;
-		int time=0;
+	public static int[] EDF(HDD disk, int pos, int t){
+		int currentPos=pos;
+		int time=t;
 		int rejected=0;
 		int[] result=new int[3];
-		while(disk.containsDeadlines()){
-			Rekord nearest=disk.shortestDeadline();
+		while(disk.containsDeadlines(time)){
+			Rekord nearest=disk.shortestDeadline(time);
 			int deadlinePos=nearest.getPos();
 			if(nearest.getDeadline()>=Math.abs(deadlinePos-currentPos)){
 				time+=Math.abs(deadlinePos-currentPos);
 				currentPos=deadlinePos;
 				disk.removeRefAt(deadlinePos);
+				disk.updateDeadlines(time);
 			}
 			else{
 				rejected++;
@@ -245,30 +323,33 @@ public class Main {
 	}
 	
 	//wynikiem jest trzyelementowa tablica zawieraj¹ca w komórce 0 czas realizacji, liczbê odrzuconych zadañ w komórce 1 i ostatni¹ pozycjê g³owic w komórce 2
-	public static int[] FDSCAN(HDD disk){
+	public static int[] FDSCAN(HDD disk, int pos, int t){
 		int[] result=new int[3];
-		int time=0;
+		int time=t;
 		int rejected=0;
 		Rekord nearest;
-		int currentPos=disk.defaultPos;
-		while(disk.containsDeadlines()){
-			nearest=disk.nearestDeadline(currentPos);
+		int currentPos=pos;
+		while(disk.containsDeadlines(time)){
+			nearest=disk.nearestDeadline(currentPos, time);
+				
 			int deadlinePos=nearest.getPos();
 			//System.out.println(deadlinePos);
 			if (nearest.getDeadline()>=Math.abs(currentPos-deadlinePos))	//je¿eli czas przejœcia do celu jest mniejszy ni¿ jego deadline
 				if (deadlinePos<currentPos)
 					// kiedy deadline znajduje siê ni¿ej pozycji bie¿¹cej
 					for (int i=currentPos; i>=deadlinePos; i--){
-						if(disk.removeRefAt(i)){
+						if(disk.removeRefAt(i, time)){
 							time+=Math.abs(currentPos-i);
+							disk.updateDeadlines(time);
 							currentPos=i;
 						}
 					}
 				else
 					//kiedy deadline znajduje siê wy¿ej pozycji bie¿¹cej
 					for (int i=currentPos; i<=deadlinePos; i++){
-						if(disk.removeRefAt(i)){
+						if(disk.removeRefAt(i, time)){
 							time+=Math.abs(currentPos-i);
+							disk.updateDeadlines(time);
 							currentPos=i;
 						}
 					}
@@ -278,6 +359,7 @@ public class Main {
 				disk.removeRefAt(deadlinePos);
 			//System.out.println(rejected);
 			}
+				
 		}
 		result[0]=time;
 		result[1]=rejected;
--- src/Rekord.java
+++ src/Rekord.java
@@ -3,15 +3,19 @@ public class Rekord {
 	
 	private int pos;
 	private int deadline;
+	private int arrival;
 	
-	public Rekord(int pos, int deadline){		//rekord dla aplikacji real-time z deadlinem
+	
+	public Rekord(int pos, int deadline, int arrival){		//rekord dla aplikacji real-time z deadlinem
 		this.pos=pos;
 		this.deadline=deadline;
+		this.arrival=arrival;
 	}
 	
 	public Rekord(int pos){ 	//zwyk³y rekord, bez deadlinu
 		this.pos=pos;
 		deadline=0;
+		arrival=0;
 	}
 	
 	public int getPos(){
@@ -22,4 +26,11 @@ public class Rekord {
 		return deadline;
 	}
 	
+	public void setDeadline(int deadline){
+		this.deadline=deadline;
+	}
+	
+	public int getArrival(){
+		return arrival;
+	}
 }
